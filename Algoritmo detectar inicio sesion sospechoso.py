af09b8c794b6a7bd5fad1beee8f2e7eaf8177c8d
Mila <115313493+Milamagof@users.noreply.github.com>
2023-10-21 19:20:52 +0200

    Add files via upload

---
+++ Algoritmo detectar inicio sesion sospechoso.py
@@ -0,0 +1,29 @@
+"""Programa que se ejecute cada momento en que
+un nuevo usuario inicia sesión y verifique si ese usuario ha tenido
+tres o más intentos fallidos de inicio de sesión """
+
+# Abrir, leer y split el archivo de texto
+
+with open("usuarios_inicio_sesion.txt", "r", encoding="utf8") as archivo:
+    archivo_texto = archivo.read()
+usernames = archivo_texto.split()
+
+
+# Contar los intentos fallidos de inicio de sesión de un usuario
+
+def login_check(login_list, usuario_actual):
+    if usuario_actual in login_list:
+        counter = 0
+        for i in login_list:
+            if i == usuario_actual:
+                counter = counter + 1
+
+        if counter >= 3:
+            return "Haz hecho 3 o más intentos de inicio de sesión.Tu cuenta ha sido bloqueada"
+        else:
+            return "Estás logueado"
+    else:
+        return "Usuario inexistente"
+
+
+print(login_check(usernames, "López"))
